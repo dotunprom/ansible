1ef60bdc9f301127c87198092eadf329c6683080
dotunakin <dotunprom@gmail.com>
2022-03-28 15:39:23 +0100

    route53

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name, Values=Centos-7-Devops-Practice" | jq '.image [].ImageID' | sed -e 's/"//g')
+if [ -z "$1" ]; then
+  echo -e "\e[31mInput Machine Name is needed\e[0m"
+  exit
+fi
+
+COMPONENT=$1
+ZONE_ID="Z0162740WGIXNEZAQ2EF"
+
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 
 echo $AMI_ID
-aws ec2 run-instrances --image-id ${AMI_ID} --instance-type t2.micro
+PRIVATE_IP=$(aws ec2 run-instances \
+  --image-id ${AMI_ID} \
+  --instance-type t2.micro \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
+  --security-group-ids ${SGID} \
+  | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
